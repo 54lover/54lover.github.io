203b8b70b26b92f7d45c250ddacd19101de20768
54lover <jdjdbrbjfjeb@gmail.com>
2025-11-15 19:10:42 +0100

    Add files via upload

---
+++ Phrase.js
@@ -0,0 +1,24 @@
+var phrases=[
+
+    "Fais gaffe à tes meufs.",
+
+    "7 Rue des Lions ,77400 Lagny-sur-Marne.",
+
+    "Ton heure arrive, regarde bien la date.",
+    
+    "ip_adress : b51f:d68e:5b2b:b8d5:e4f9:7c07:06ad:5953",
+        
+    "90.121.23.167",
+
+    "Tu crois être une bonne personne Zazu ?",
+
+    "Tu vas très bientôt avoir ton downfall.",
+
+    "Tu es un pédophile ZAZU.",
+
+    "31 ans et 16 ans ce n'est pas NORMAL.",
+
+    "J'ai des dossiers importants sur toi.",
+
+    "Regarde bien partout quand tu sors."
+]
